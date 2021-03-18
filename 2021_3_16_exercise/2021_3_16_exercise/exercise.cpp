2f3268b17725bf055f68d72b118645ad864c2b7e
yangxy01 <1271380537@qq.com>
2021-03-18 23:26:17 +0800

    2021_3_18

--- 2021_3_16_exercise/2021_3_16_exercise/exercise.cpp
+++ 2021_3_16_exercise/2021_3_16_exercise/exercise.cpp
@@ -1,6 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
+
+//void my_strcpy(const char*arr1, char*arr2)
+//{
+//	while (*arr1)
+//	{
+//		*arr2 = *arr1;
+//		arr1++;
+//		arr2++;
+//	}
+//	*arr2 = *arr1;
+//}
+//
+//int main()//×Ö·û´®¸´ÖÆ
+//{
+//	char arr1[20] = "abcdefg";
+//	char arr2[20];
+//	my_strcpy(arr1, arr2);
+//	cout << arr2 << endl;
+//	return 0;
+//}
+
+
 int main()//ÊµÏÖ×Ö·û´®ÄæÐòÊä³ö
 {
 	int n;
